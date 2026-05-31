8bd33709e6f01b1f72cd572f6da9e91b980a4c6a
TasinLunatic <ahmedtasin207@gmail.com>
2026-05-31 20:48:39 +0800

    remove some unnecessary comments in router

--- schedule-management/src/router/router.js
+++ schedule-management/src/router/router.js
@@ -1,57 +1,55 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from "vue-router";
 
-// Import components
-import Login from '../components/Login.vue'
-import Regist from '../components/Regist.vue'
-import ShowSchedule from '../components/ShowSchedule.vue'
+import Login from "../components/Login.vue";
+import Regist from "../components/Regist.vue";
+import ShowSchedule from "../components/ShowSchedule.vue";
 
-// Import Pinia instance and user store
-import pinia from '../pinia.js'
-import { defineUser } from '../store/userStore.js'
+import pinia from "../pinia.js";
+import { defineUser } from "../store/userStore.js";
 
-const sysUser = defineUser(pinia)
+const sysUser = defineUser(pinia);
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
-      path: '/',
-      redirect: '/showSchedule'
+      path: "/",
+      redirect: "/showSchedule",
     },
     {
-      path: '/showSchedule',
-      component: ShowSchedule
+      path: "/showSchedule",
+      component: ShowSchedule,
     },
     {
-      path: '/login',
-      component: Login
+      path: "/login",
+      component: Login,
     },
     {
-      path: '/regist',
-      component: Regist
-    }
-  ]
-})
+      path: "/regist",
+      component: Regist,
+    },
+  ],
+});
 
 // Global Navigation Guard
 router.beforeEach((to, from, next) => {
-  if (to.path === '/showSchedule') {
-    const loginUserFromStorage = sessionStorage.getItem('loginUser')
+  if (to.path === "/showSchedule") {
+    const loginUserFromStorage = sessionStorage.getItem("loginUser");
 
     if (loginUserFromStorage) {
       // Restore user data into Pinia if it's missing
       if (!sysUser.username) {
-        sysUser.username = loginUserFromStorage
-        sysUser.uid = 1
+        sysUser.username = loginUserFromStorage;
+        sysUser.uid = 1;
       }
-      next()   // ✅ Allow access to My Schedule
+      next();
     } else {
-      alert('Please log in first.')
-      next('/login')   // Redirect to login page
+      alert("Please log in first.");
+      next("/login");
     }
   } else {
-    next()   // Login and Register pages are always accessible
+    next();
   }
-})
+});
 
-export default router
+export default router;
