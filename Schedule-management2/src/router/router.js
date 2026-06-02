b96de5f06c143c52d5184be13750286023528346
TasinLunatic <ahmedtasin207@gmail.com>
2026-06-02 17:42:43 +0800

    new router  added

--- Schedule-management2/src/router/router.js
+++ Schedule-management2/src/router/router.js
@@ -1,55 +1,44 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from 'vue-router'
 
-import Login from "../components/Login.vue";
-import Regist from "../components/Regist.vue";
-import ShowSchedule from "../components/ShowSchedule.vue";
-
-import pinia from "../pinia.js";
-import { defineUser } from "../store/userStore.js";
-
-const sysUser = defineUser(pinia);
+// Import Components
+import Login from './components/Login.vue'
+import Regist from './components/Regist.vue'
+import ShowSchedule from './components/ShowSchedule.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
-      path: "/",
-      redirect: "/showSchedule",
+      path: '/', 
+      redirect: '/login'        // Default page is Login
     },
     {
-      path: "/showSchedule",
-      component: ShowSchedule,
+      path: '/login',
+      component: Login
     },
     {
-      path: "/login",
-      component: Login,
+      path: '/regist',
+      component: Regist
     },
     {
-      path: "/regist",
-      component: Regist,
-    },
-  ],
-});
-
-// Global Navigation Guard
-router.beforeEach((to, from, next) => {
-  if (to.path === "/showSchedule") {
-    const loginUserFromStorage = sessionStorage.getItem("loginUser");
-
-    if (loginUserFromStorage) {
-      // Restore user data into Pinia if it's missing
-      if (!sysUser.username) {
-        sysUser.username = loginUserFromStorage;
-        sysUser.uid = 1;
-      }
-      next();
-    } else {
-      alert("Please log in first.");
-      next("/login");
+      path: '/showSchedule',
+      component: ShowSchedule
     }
-  } else {
-    next();
-  }
-});
+  ]
+})
 
-export default router;
+// Simple Navigation Guard
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = sessionStorage.getItem('loginUser')
+
+  // If trying to access schedule without login → redirect to login
+  if (to.path === '/showSchedule' && !isLoggedIn) {
+    alert('Please login first!')
+    next('/login')
+  } 
+  else {
+    next()   // Allow access
+  }
+})
+
+export default router
